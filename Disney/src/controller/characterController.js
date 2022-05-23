8ef660e037121af790a20b60a3d1eec17a4d2d37
AyrtonRada <axel582011@hotmail.com>
2022-05-23 02:15:20 -0300

    CRUD personajes

--- Disney/src/controller/characterController.js
+++ Disney/src/controller/characterController.js
@@ -3,7 +3,9 @@ const db = require('../database/models/Index')
 
 const characterController = {
 
+     /*********LISTA DE PERSONAJES *********/
     characters: async (req,res) => {
+        //encontrar todos los personajes para mostrar imagen y nombre
          await db.Personaje.findAll()
          .then((personaje)=> {
              res.send({
@@ -12,10 +14,66 @@ const characterController = {
                 })
          })
     },
-    
+     /*********CREAR PERSONAJE *********/
+    charactersCreate: async(req,res) => {
+        //validacion de existencia de los datos para crear personaje
+        const errores = validationResult(req)
+        
+        if (errores.errors.length > 0 ) {
+            return res.send( {errors: errores.mapped()})
+        }
 
+        //crear un personaje
+        await db.Personaje.create({
+            imagen: req.body.imagen,
+            nombre: req.body.nombre,
+            edad: req.body.edad,
+            peso: req.body.peso,
+            history: req.body.history,
+            pelicula_serie_asociada: req.body.pelicula_serie_asociada
+        })
+        .then(()=> {
+            res.send('Personaje creado')
+        })
+    },
 
+    /*********ACTUALIZAR PERSONAJE *********/
+    charactersUpdate: async(req,res) =>{
+      //encontrar el personaje a editar
+      await db.Personaje.findByPk(req.params.id)
+      
+      //validacion de existencia de los datos para editar personaje
+      const errores = validationResult(req)
+        
+      if (errores.errors.length > 0 ) {
+          return res.send( {errors: errores.mapped()})
+      }
 
+      //editar personaje
+      await db.Personaje.update({
+        imagen: req.body.imagen,
+        nombre: req.body.nombre,
+        edad: req.body.edad,
+        peso: req.body.peso,
+        history: req.body.history,
+        pelicula_serie_asociada: req.body.pelicula_serie_asociada
+      }),{
+          where: {id: req.params.id}
+      }
+      .then(()=> {
+          res.send(`Personaje con id: ${req.params.id} actualizado`)
+      })
+    },
+
+     /*********ELIMINAR PERSONAJE *********/
+     charactersDestroy: async(req,res) => {
+        await db.Personaje.destroy({
+            where: {id: req.params.id}
+        })
+        .then(()=>{
+            res.send(`Personaje con id: ${req.params.id} eliminado`)
+        })
+     }
 }
 
 module.exports = characterController
--- Disney/src/routes/character.js
+++ Disney/src/routes/character.js
@@ -1,8 +1,14 @@
 let express = require('express')
 let router = express.Router()
 const characterController = require('../controller/characterController')
+const validacionesCharacter = require('../middleware/characterMiddleware')
+const uploadFile = require('../middleware/multerMiddleware')
+const verificarToken = require('../middleware/authTokenMiddleware')
 
-
-
+//localhost:3000/characters
+router.get('/', characterController.characters)
+router.post('/Create', uploadFile.single('imagen'), validacionesCharacter, characterController.charactersCreate)
+router.put('/Update/:id', uploadFile.single('imagen'), validacionesCharacter, characterController.charactersUpdate)
+router.delete('/Delete/:id', characterController.charactersDestroy)
 
 module.exports = router
